7c85785a9b200587d50af9ea436751976a1dce15
unknown <hlataoui@wd0710006.vmf.headoffice.local>
2015-05-22 15:50:07 +0200

    test espace

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Annonces\UtilisateursBundle\AnnoncesUtilisateursBundle(),
             new Annonces\SiteBundle\AnnoncesSiteBundle(),
+            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
